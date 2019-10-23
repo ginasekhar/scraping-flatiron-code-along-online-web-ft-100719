6fc5ea656ad43c005060397548a34f5159fc2d03
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-23 19:23:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,8 @@ class Scraper
   end #print_courses
   
   def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end
   
 end
